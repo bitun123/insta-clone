4ddbed2c3890953baa9fd17fc8e0ff501e36cb09
satyajit <satyajitdasmahapatra2002@gmail.com>
2026-02-17 22:57:35 +0530

    create more api that getpost details post

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -2,6 +2,7 @@ const postModels = require("../models/post.models");
 const ImageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
 const jwt = require("jsonwebtoken");
+const { post } = require("../routes/post.routes");
 
 const imageKit = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -44,20 +45,81 @@ async function createPostControllers(req, res) {
   });
 }
 
-async function getPostControllers(req,res){
-const token  = req.cookies.tokens;
-if(!token){
+async function getPostControllers(req, res) {
+  const token = req.cookies.token;
+  if (!token) {
+    res.status(401).json({
+      message: "unauthorized user",
+    });
+  }
+  let decode = null;
+
+  try {
+    decode = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    res.status(401).json({
+      message: "token Invalid ",
+    });
+  }
+  console.log(decode);
+  const userId = decode.id;
+  const posts = await postModels.find({
+    user: userId,
+  });
+  if (posts.length == 0) {
+    return res.status(401).json({
+      message: "there no post you created",
+    });
+  }
+  res.status(200).json({
+    message: "post fetch successfully",
+    posts,
+  });
+}
+
+async function getPostDetailsControllers(req, res) {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "unauthorized access",
+    });
+  }
+let decode = null;
+try {
+  decode = jwt.verify(token,process.env.JWT_SECRET)
+} catch (error) {
   res.status(401).json({
-    message:"unthorized user"
+    message :"Invalid User"
   })
 }
 
 
+const userId  = decode.id;
+const postId = req.params.postId;
+const post = await postModels.findById(postId);
+if(!post){
+  return res.status(404).json({
+    message:"there are no post"
+  })
+}
+
+const isValidUser = post.user.toString() === userId;
+if(!isValidUser){
+  return res.status(403).json({
+    message :"forbidden content "
+  })
 }
 
 
+return res.status(200).json({
+  message:"post Fetch Successfully",
+  post
+})
+
+}
 
 module.exports = {
-createPostControllers,
-  getPostControllers
+  createPostControllers,
+  getPostControllers,
+  getPostDetailsControllers,
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,6 +6,8 @@ const postControllers = require("../controllers/post.controllers");
 
 postRouter.post("/", upload.single("image"), postControllers.createPostControllers);
 
-postRouter.get("/",postControllers.getPostControllers)
+postRouter.get("/",postControllers.getPostControllers);
+
+postRouter.get("/details/:postId",postControllers.getPostDetailsControllers)
 
 module.exports = postRouter;
